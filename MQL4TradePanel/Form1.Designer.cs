75f61fa2544a8f7c3eb259a6bdd47f2c05179a20
group14 <group14@19-PK002>
2022-01-18 22:16:08 +0900

    プロジェクト ファイルを追加します。

---
+++ MQL4TradePanel/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MQL4TradePanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buyBtn1 = new System.Windows.Forms.Button();
+            this.sellBtn1 = new System.Windows.Forms.Button();
+            this.connect = new System.Windows.Forms.Button();
+            this.disconnect = new System.Windows.Forms.Button();
+            this.getTeminal = new System.Windows.Forms.Button();
+            this.bidTbx = new System.Windows.Forms.TextBox();
+            this.askTbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buyBtn1
+            // 
+            this.buyBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buyBtn1.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buyBtn1.Location = new System.Drawing.Point(12, 92);
+            this.buyBtn1.Name = "buyBtn1";
+            this.buyBtn1.Size = new System.Drawing.Size(107, 40);
+            this.buyBtn1.TabIndex = 2;
+            this.buyBtn1.Text = "button1";
+            this.buyBtn1.UseVisualStyleBackColor = true;
+            this.buyBtn1.Click += new System.EventHandler(this.buyBtn1_Click);
+            // 
+            // sellBtn1
+            // 
+            this.sellBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sellBtn1.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.sellBtn1.Location = new System.Drawing.Point(134, 92);
+            this.sellBtn1.Name = "sellBtn1";
+            this.sellBtn1.Size = new System.Drawing.Size(107, 40);
+            this.sellBtn1.TabIndex = 3;
+            this.sellBtn1.Text = "button2";
+            this.sellBtn1.UseVisualStyleBackColor = true;
+            this.sellBtn1.Click += new System.EventHandler(this.sellBtn1_Click);
+            // 
+            // connect
+            // 
+            this.connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connect.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.connect.Location = new System.Drawing.Point(90, 12);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(72, 34);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "接続";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_ClickAsync);
+            // 
+            // disconnect
+            // 
+            this.disconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.disconnect.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.disconnect.Location = new System.Drawing.Point(169, 12);
+            this.disconnect.Name = "disconnect";
+            this.disconnect.Size = new System.Drawing.Size(72, 34);
+            this.disconnect.TabIndex = 1;
+            this.disconnect.Text = "切断";
+            this.disconnect.UseVisualStyleBackColor = true;
+            this.disconnect.Click += new System.EventHandler(this.disconnect_Click);
+            // 
+            // getTeminal
+            // 
+            this.getTeminal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.getTeminal.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.getTeminal.Location = new System.Drawing.Point(12, 12);
+            this.getTeminal.Name = "getTeminal";
+            this.getTeminal.Size = new System.Drawing.Size(72, 34);
+            this.getTeminal.TabIndex = 4;
+            this.getTeminal.Text = "一覧取得";
+            this.getTeminal.UseVisualStyleBackColor = true;
+            this.getTeminal.Click += new System.EventHandler(this.getTeminal_Click);
+            // 
+            // bidTbx
+            // 
+            this.bidTbx.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bidTbx.Location = new System.Drawing.Point(12, 52);
+            this.bidTbx.Name = "bidTbx";
+            this.bidTbx.Size = new System.Drawing.Size(107, 34);
+            this.bidTbx.TabIndex = 5;
+            // 
+            // askTbx
+            // 
+            this.askTbx.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.askTbx.Location = new System.Drawing.Point(134, 52);
+            this.askTbx.Name = "askTbx";
+            this.askTbx.Size = new System.Drawing.Size(107, 34);
+            this.askTbx.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.askTbx);
+            this.Controls.Add(this.bidTbx);
+            this.Controls.Add(this.getTeminal);
+            this.Controls.Add(this.disconnect);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.sellBtn1);
+            this.Controls.Add(this.buyBtn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buyBtn1;
+        private System.Windows.Forms.Button sellBtn1;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button disconnect;
+        private System.Windows.Forms.Button getTeminal;
+        private System.Windows.Forms.TextBox bidTbx;
+        private System.Windows.Forms.TextBox askTbx;
+    }
+}
+
---
+++ MQL4TradePanel/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace MQL4TradePanel
+{
+    public partial class Form1 : Form
+    {
+
+
+        //TradePanel用DPI調整係数
+        static readonly float DpiScale = ((new System.Windows.Forms.Form()).CreateGraphics().DpiX) / 96;
+
+        //高DPI対応についてはこの辺
+        //https://qiita.com/mono1729/items/a93505a5cb3fe194b7dc
+
+        //socket通信についてはこの辺
+        //https://itsakura.com/csharp-socket
+        //https://qiita.com/_T_E_T_R_A_/items/bdd310714fea63474a7b
+
+
+        TcpListener server = null;
+        IPHostEntry ipHostInfo;
+        IPAddress ipAddress;
+        int port = 4545;
+
+        bool startFlg = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getTeminal_Click(object sender, EventArgs e)
+        {
+            //ローカルコンピュータ上で実行されている"notepad"という名前の
+            //すべてのプロセスを取得
+            System.Diagnostics.Process[] ps =
+                System.Diagnostics.Process.GetProcessesByName("terminal");
+
+            //配列から1つずつ取り出す
+            foreach (System.Diagnostics.Process p in ps)
+            {
+                //IDとメインウィンドウのキャプションを出力する
+                Console.WriteLine("{0}/{1}", p.Id, p.MainWindowTitle);
+                connect.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            //connect.Enabled = false;
+            //disconnect.Enabled = false;
+           
+            startFlg = true;
+        }
+
+
+        private void connect_ClickAsync(object sender, EventArgs e)
+        {
+            ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in ipHostInfo.AddressList)
+            {
+                Console.WriteLine(ip.ToString());
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+
+                    ipAddress = ip;
+                    //break;
+                }
+            }
+            //ipAddress = ipHostInfo.AddressList[0];
+            ipAddress = IPAddress.Parse("127.0.0.1");
+            Task task = Task.Run(async () => { await SocketServer(); });
+
+            //接続が成功したら
+            //disconnect.Enabled = true;
+            //connect.Enabled = false;
+        }
+        private void disconnect_Click(object sender, EventArgs e)
+        {
+            startFlg = false;
+
+            //切断したら
+            // disconnect.Enabled = false;
+            //connect.Enabled = true;
+
+            //ソケットの終了
+            //server.Stop();
+            Console.WriteLine("サーバー側終了です");
+        }
+
+        public async Task SocketServer()
+        {
+            //ここからIPアドレスやポートの設定
+            // 着信データ用のデータバッファー。
+            byte[] bytes = new byte[1024];
+
+            await Task.Run(() =>
+            {
+                try
+                {
+
+                    Regex reg = new Regex("\0");
+                    if (server == null)
+                    {
+                        //指定IP 指定ポートでサーバを起動
+                        server = new TcpListener(new IPEndPoint(ipAddress, port));
+                    }
+                    server.Start();
+                    while (startFlg)
+                    {
+                        if (!startFlg)
+                        {
+                            server.Stop();
+                            break;
+                        }
+
+                        using (TcpClient tcpClient = server.AcceptTcpClient())
+                        using (Stream stream = tcpClient.GetStream())
+                        {
+                            int i = 0;
+                            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                            {
+                                string data1 = reg.Replace(Encoding.UTF8.GetString(bytes), "");
+
+
+
+                                // 任意の処理
+                                //データの受取をReceiveで行う。
+                                //int bytesRec = handler.Receive(bytes);
+                                //string data1 = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                                if (data1.Length > 0)
+                                {
+                                    Console.WriteLine("data1の数:" + data1.Length);
+                                    Console.WriteLine(data1);
+
+                                    String[] sep = { "," };
+                                    String[] sba = data1.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+                                    if (this.InvokeRequired)
+                                    {
+                                        //別スレッドによるUI操作
+                                        this.Invoke((MethodInvoker)(() =>
+                                        {
+                                            bidTbx.Text = sba[1];
+                                            askTbx.Text = sba[2];
+                                        }));
+                                    }
+                                    else
+                                    {
+                                        //UIスレッドからのUI操作
+                                        bidTbx.Text = sba[1];
+                                        askTbx.Text = sba[2];
+                                    }
+
+
+
+
+                                    //大文字に変更
+                                    data1 = data1.ToUpper();
+
+                                    //クライアントにSendで返す。
+                                    byte[] msg = Encoding.UTF8.GetBytes(data1);
+                                    stream.Write(msg, 0, msg.Length);
+                                    Array.Clear(bytes, 0, bytes.Length);
+                                }
+                            }
+                        }
+
+                        // break;
+                    }
+                }
+                catch (Exception ex)
+
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+            });
+
+        }
+
+
+        public static void SocketClient(string st)
+        {
+            //IPアドレスやポートを設定している。
+            IPHostEntry ipHostInfo1 = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress1 = ipHostInfo1.AddressList[0];
+
+            foreach (IPAddress ip in ipHostInfo1.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ipAddress1 = ip;
+                    break;
+                }
+            }
+
+            ipAddress1 = IPAddress.Parse("127.0.0.1");
+
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress1, 4545);
+
+            //ソケットを作成
+            Socket socket = new Socket(ipAddress1.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //接続する。失敗するとエラーで落ちる。
+            socket.Connect(remoteEP);
+
+            //Sendで送信している。
+            byte[] msg = Encoding.UTF8.GetBytes(st + "<EOF>");
+            socket.Send(msg);
+
+            //Receiveで受信している。
+            byte[] bytes = new byte[1024];
+            int bytesRec = socket.Receive(bytes);
+            string data1 = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            Console.WriteLine(data1);
+
+            //ソケットを終了している。
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        private void buyBtn1_Click(object sender, EventArgs e)
+        {
+
+            SocketClient("buy,141.25,,,OK");
+        }
+
+        private void sellBtn1_Click(object sender, EventArgs e)
+        {
+
+            SocketClient("sell,141.25,,,OK");
+        }
+
+        delegate void UpDateRateDelegate();
+
+        void UpDateRate(String st)
+        {
+            if (InvokeRequired)
+            {
+                // 別スレッドから呼び出された場合
+                //Invoke(new UpDateRateDelegate(UpDateRate));
+                return;
+            }
+            askTbx.Focus();
+        }
+    }
+}
