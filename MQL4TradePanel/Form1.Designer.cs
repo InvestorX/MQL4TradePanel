c1f8da6869bb9c2051ca4dbb742f99b0098dc1ce
InestroX <kinoco11+git@gmail.com>
2022-02-07 11:45:29 +0900

    update

--- MQL4TradePanel/Form1.Designer.cs
+++ MQL4TradePanel/Form1.Designer.cs
@@ -35,37 +35,56 @@
             this.getTeminal = new System.Windows.Forms.Button();
             this.bidTbx = new System.Windows.Forms.TextBox();
             this.askTbx = new System.Windows.Forms.TextBox();
+            this.spTbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.allCloseBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buyBtn1
             // 
+            this.buyBtn1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.buyBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buyBtn1.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.buyBtn1.Location = new System.Drawing.Point(12, 92);
+            this.buyBtn1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buyBtn1.Location = new System.Drawing.Point(8, 184);
             this.buyBtn1.Name = "buyBtn1";
-            this.buyBtn1.Size = new System.Drawing.Size(107, 40);
+            this.buyBtn1.Size = new System.Drawing.Size(128, 80);
             this.buyBtn1.TabIndex = 2;
-            this.buyBtn1.Text = "button1";
-            this.buyBtn1.UseVisualStyleBackColor = true;
+            this.buyBtn1.Text = "SELL";
+            this.buyBtn1.UseVisualStyleBackColor = false;
             this.buyBtn1.Click += new System.EventHandler(this.buyBtn1_Click);
             // 
             // sellBtn1
             // 
+            this.sellBtn1.BackColor = System.Drawing.Color.Tomato;
             this.sellBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.sellBtn1.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.sellBtn1.Location = new System.Drawing.Point(134, 92);
+            this.sellBtn1.Location = new System.Drawing.Point(224, 184);
             this.sellBtn1.Name = "sellBtn1";
-            this.sellBtn1.Size = new System.Drawing.Size(107, 40);
+            this.sellBtn1.Size = new System.Drawing.Size(128, 80);
             this.sellBtn1.TabIndex = 3;
-            this.sellBtn1.Text = "button2";
-            this.sellBtn1.UseVisualStyleBackColor = true;
+            this.sellBtn1.Text = "BUY";
+            this.sellBtn1.UseVisualStyleBackColor = false;
             this.sellBtn1.Click += new System.EventHandler(this.sellBtn1_Click);
             // 
             // connect
             // 
             this.connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.connect.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.connect.Location = new System.Drawing.Point(90, 12);
+            this.connect.Location = new System.Drawing.Point(144, 8);
             this.connect.Name = "connect";
             this.connect.Size = new System.Drawing.Size(72, 34);
             this.connect.TabIndex = 0;
@@ -77,7 +96,7 @@
             // 
             this.disconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.disconnect.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.disconnect.Location = new System.Drawing.Point(169, 12);
+            this.disconnect.Location = new System.Drawing.Point(280, 8);
             this.disconnect.Name = "disconnect";
             this.disconnect.Size = new System.Drawing.Size(72, 34);
             this.disconnect.TabIndex = 1;
@@ -89,7 +108,7 @@
             // 
             this.getTeminal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.getTeminal.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.getTeminal.Location = new System.Drawing.Point(12, 12);
+            this.getTeminal.Location = new System.Drawing.Point(8, 8);
             this.getTeminal.Name = "getTeminal";
             this.getTeminal.Size = new System.Drawing.Size(72, 34);
             this.getTeminal.TabIndex = 4;
@@ -100,24 +119,220 @@
             // bidTbx
             // 
             this.bidTbx.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.bidTbx.Location = new System.Drawing.Point(12, 52);
+            this.bidTbx.Location = new System.Drawing.Point(8, 144);
             this.bidTbx.Name = "bidTbx";
-            this.bidTbx.Size = new System.Drawing.Size(107, 34);
+            this.bidTbx.Size = new System.Drawing.Size(128, 34);
             this.bidTbx.TabIndex = 5;
+            this.bidTbx.Text = "12345.69";
             // 
             // askTbx
             // 
             this.askTbx.Font = new System.Drawing.Font("Meiryo UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.askTbx.Location = new System.Drawing.Point(134, 52);
+            this.askTbx.Location = new System.Drawing.Point(224, 144);
             this.askTbx.Name = "askTbx";
-            this.askTbx.Size = new System.Drawing.Size(107, 34);
+            this.askTbx.Size = new System.Drawing.Size(128, 34);
             this.askTbx.TabIndex = 5;
+            this.askTbx.Text = "42198.68";
+            this.askTbx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // spTbx
+            // 
+            this.spTbx.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.spTbx.Location = new System.Drawing.Point(144, 184);
+            this.spTbx.Name = "spTbx";
+            this.spTbx.Size = new System.Drawing.Size(64, 28);
+            this.spTbx.TabIndex = 6;
+            this.spTbx.Text = "10000";
+            this.spTbx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(8, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bid";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(320, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ask";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(144, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Spread";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(32, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(296, 35);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "通貨";
+            // 
+            // allCloseBtn
+            // 
+            this.allCloseBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.allCloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.allCloseBtn.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.allCloseBtn.Location = new System.Drawing.Point(144, 224);
+            this.allCloseBtn.Name = "allCloseBtn";
+            this.allCloseBtn.Size = new System.Drawing.Size(72, 40);
+            this.allCloseBtn.TabIndex = 11;
+            this.allCloseBtn.Text = "全決済";
+            this.allCloseBtn.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(8, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 19);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "取引数量(Lots)";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(96, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "▲";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(96, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 32);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "▼";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Meiryo UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(8, 296);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 68);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "50";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(144, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 32);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "+1";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(144, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(64, 32);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "-1";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(216, 336);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 32);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "-5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button6.Location = new System.Drawing.Point(216, 296);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(64, 32);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "+5";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Control;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button7.Location = new System.Drawing.Point(288, 336);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(64, 32);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "-10";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Control;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button8.Location = new System.Drawing.Point(288, 296);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(64, 32);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "+10";
+            this.button8.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(365, 601);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.allCloseBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.spTbx);
             this.Controls.Add(this.askTbx);
             this.Controls.Add(this.bidTbx);
             this.Controls.Add(this.getTeminal);
@@ -125,9 +340,10 @@
             this.Controls.Add(this.connect);
             this.Controls.Add(this.sellBtn1);
             this.Controls.Add(this.buyBtn1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "激速注文 for MetaTrader @InvestorX";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -143,6 +359,22 @@
         private System.Windows.Forms.Button getTeminal;
         private System.Windows.Forms.TextBox bidTbx;
         private System.Windows.Forms.TextBox askTbx;
+        private System.Windows.Forms.TextBox spTbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button allCloseBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
